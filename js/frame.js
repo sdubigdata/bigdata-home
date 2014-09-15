c23ecf3954b37d6d9d8ece166fd3dd57bbb66cbb
xccui <xingcanc@gmail.com>
2014-09-15 21:12:21 +0800

    完善网站结构，修正了低版本IE下的显示错误

--- js/frame.js
+++ js/frame.js
@@ -1,4 +1,8 @@
 window.onload = function(){
+  setSize();
+}
+
+function setSize(){
   parent.document.getElementsByName("main_frame")[0].style.height = "auto";
   var height = document.body.scrollHeight;
   parent.document.getElementsByName("main_frame")[0].style.height = (height + "px");
