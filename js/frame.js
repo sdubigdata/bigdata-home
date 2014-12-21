5ff45efb76d2942da50971babe824efc10b199df
yestinchan <873915231@qq.com>
2014-12-21 15:51:41 +0800

    move pure static files to jekyll

--- js/frame.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-  setSize();
-}
-
-function setSize(){
-  parent.document.getElementsByName("main_frame")[0].style.height = "auto";
-  var height = document.body.scrollHeight;
-  parent.document.getElementsByName("main_frame")[0].style.height = (height + "px");
-}
